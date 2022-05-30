e420af70a9bcc2e0b29cc09e23afc8aa204b3c21
lima-anderson <msgli34@gmail.com>
2022-05-29 23:57:35 -0300

    first commit

---
+++ src/test/java/com/pdist/cadastroalunos/model/AlunoTest.java
@@ -0,0 +1,14 @@
+package com.pdist.cadastroalunos.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AlunoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
